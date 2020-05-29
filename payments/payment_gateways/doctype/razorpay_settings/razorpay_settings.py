778472666cb61200ab00b30a18074d66ddb0bc8f
Shivam Mishra <scm.mymail@gmail.com>
2020-05-29 13:22:52 +0530

    refactor: use six instead of sys.version_info

--- payments/payment_gateways/doctype/razorpay_settings/razorpay_settings.py
+++ payments/payment_gateways/doctype/razorpay_settings/razorpay_settings.py
@@ -636,7 +636,7 @@ class RazorpaySettings(Document):
 			frappe.log_error(frappe.get_traceback())
 
 	def verify_signature(self, body, signature, key):
-		if sys.version_info[0] == 3:
+		if six.PY3:
 			key = bytes(key, 'utf-8')
 			body = bytes(body, 'utf-8')
 
@@ -646,7 +646,7 @@ class RazorpaySettings(Document):
 
 		generated_signature = dig.hexdigest()
 
-		if sys.version_info[0:3] < (2, 7, 7):
+		if six.PY2:
 			result = self.compare_string(generated_signature, signature)
 		else:
 			result = hmac.compare_digest(generated_signature, signature)
