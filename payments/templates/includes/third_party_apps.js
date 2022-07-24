bbe90ec8b87bc57b2c3877e9e927f58c9fc02cbc
phot0n <ritwikpuri5678@gmail.com>
2022-07-24 20:07:50 +0530

    chore: remove gcalendar template and third party apps template include

--- payments/templates/includes/third_party_apps.js
+++
@@ -1,9 +0,0 @@
-frappe.ready(() => {
-	$(".btn-delete-app").on("click", function(event) {
-		frappe.call({
-			method:"frappe.www.third_party_apps.delete_client",
-			args: {"client_id": $(this).data("client_id"),
-			}
-		}).done(r => location.href="/third_party_apps");
-	});
-});
