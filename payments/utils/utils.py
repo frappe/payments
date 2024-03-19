4b54933623297ca0c713de35e9b66dd6f01f54c7
Kevin Shenk <kevin@avu.nu>
2024-03-19 10:07:42 -0400

    refactor: re-instate make_custom_fields (for ERPNext)

--- payments/utils/utils.py
+++ payments/utils/utils.py
@@ -1,6 +1,8 @@
 import click
 import frappe
 from frappe import _
+from contextlib import contextmanager
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 
 def get_payment_gateway_controller(payment_gateway):
@@ -51,6 +53,51 @@ def create_payment_gateway(gateway, settings=None, controller=None):
 		payment_gateway.insert(ignore_permissions=True)
 
 
+def make_custom_fields():
+	if "erpnext" in frappe.get_installed_apps():
+		custom_fields = {
+			"GoCardless Mandate": [
+				{
+					"fieldname": "customer",
+					"fieldtype": "Link",
+					"in_list_view": 1,
+					"label": "Customer",
+					"options": "Customer",
+					"reqd": 1,
+					"insert_after": "disabled",
+				}
+			]
+		}
+
+		create_custom_fields(custom_fields)
+
+
+def delete_custom_fields():
+	if frappe.get_meta("Web Form").has_field("payments_tab"):
+		click.secho("* Uninstalling Payment Custom Fields from Web Form")
+
+		fieldnames = (
+			"accept_payment",
+			"amount_based_on_field",
+			"amount_field",
+			"amount",
+			"currency",
+			"payer_email_based_on_field",
+			"payer_email_field",
+			"payer_name_based_on_field",
+			"payer_name_field",
+			"payment_gateway",
+			"payments_cb",
+			"payments_sb",
+			"payments_tab"
+		)
+
+		for fieldname in fieldnames:
+			frappe.db.delete("Custom Field", {"name": "Web Form-" + fieldname})
+
+		frappe.clear_cache(doctype="Web Form")
+
+
 def before_install():
 	# TODO: remove this
 	# This is done for erpnext CI patch test
@@ -62,3 +109,16 @@ def before_install():
 	# a lot of apis don;t exist in v10 and this is a (at the moment) required app for erpnext.
 	if not frappe.get_meta("Module Def").has_field("custom"):
 		return False
+
+
+@contextmanager
+def erpnext_app_import_guard():
+	marketplace_link = '<a href="https://frappecloud.com/marketplace/apps/erpnext">Marketplace</a>'
+	github_link = '<a href="https://github.com/frappe/erpnext">GitHub</a>'
+	msg = _("erpnext app is not installed. Please install it from {} or {}").format(
+		marketplace_link, github_link
+	)
+	try:
+		yield
+	except ImportError:
+		frappe.throw(msg, title=_("Missing ERPNext App"))
