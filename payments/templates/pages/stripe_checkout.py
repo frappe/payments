bcd196eff731e9ece1a97fcbd2b9465d4afb4509
Jannat Patel <pateljannat2308@gmail.com>
2025-03-18 16:09:03 +0530

    chore: fixed linters

--- payments/templates/pages/stripe_checkout.py
+++ payments/templates/pages/stripe_checkout.py
@@ -53,8 +53,7 @@ def get_context(context):
 	else:
 		frappe.log_error(
 			"Missing keys in form_dict",
-			"Expected keys: {0},"
-			"Received keys: {1}".format(expected_keys, list(frappe.form_dict)),
+			"Expected keys: {}," "Received keys: {}".format(expected_keys, list(frappe.form_dict)),
 		)
 		frappe.redirect_to_message(
 			_("Some information is missing"),
@@ -78,7 +77,9 @@ def get_header_image(doc, gateway_controller):
 
 
 @frappe.whitelist(allow_guest=True)
-def make_payment(stripe_token_id, data, reference_doctype=None, reference_docname=None, payment_gateway=None):
+def make_payment(
+	stripe_token_id, data, reference_doctype=None, reference_docname=None, payment_gateway=None
+):
 	data = json.loads(data)
 
 	data.update({"stripe_token_id": stripe_token_id})
