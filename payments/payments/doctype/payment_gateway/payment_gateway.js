df4904e11b1d878d5e74617566c38b853fff0f58
Shllokkk <shllokosan23@gmail.com>
2026-04-27 17:01:51 +0530

    feat: move Payment Gateway Doctype into Payments from ERPNext

--- payments/payments/doctype/payment_gateway/payment_gateway.js
+++ payments/payments/doctype/payment_gateway/payment_gateway.js
@@ -2,5 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Payment Gateway", {
-  refresh: function (frm) {},
+  onload: function (frm) {
+    frm.fields_dict.payment_gateway_account.grid.get_field("payment_account").get_query = (frm, cdt, cdn) => {
+      row = locals[cdt][cdn];
+      return {
+        filters: {
+          company: row.company,
+        }
+      };
+    }
+  },
 });
