e8643a429455ff05b745711e88993af7dba92e01
mehairwamlake <mehariwamlake@gmail.com>
2026-06-26 17:34:29 +0300

    l

---
+++ payments/payment_gateways/chapa_integration.py
@@ -0,0 +1,35 @@
+import frappe
+import requests
+
+class ChapaGateway:
+
+    BASE_URL = "https://api.chapa.co/v1"
+
+    def __init__(self):
+        settings = frappe.get_single("Chapa Settings")
+
+        self.secret = settings.get_password("secret_key")
+
+        self.headers = {
+            "Authorization": f"Bearer {self.secret}"
+        }
+
+    def initialize(self, data):
+
+        payload = {
+            "amount": str(data.amount),
+            "currency": "ETB",
+            "email": data.payer_email,
+            "first_name": data.payer_name,
+            "tx_ref": data.reference_doctype + "-" + data.reference_docname,
+            "callback_url": data.callback_url,
+            "return_url": data.return_url
+        }
+
+        r = requests.post(
+            self.BASE_URL + "/transaction/initialize",
+            json=payload,
+            headers=self.headers
+        )
+
+        return r.json()
--- payments/payment_gateways/doctype/chapa_settings/chapa_settings.py
+++ payments/payment_gateways/doctype/chapa_settings/chapa_settings.py
@@ -1,290 +1,118 @@
-# Copyright (c) 2018, Frappe Technologies and contributors
-# License: MIT. See LICENSE
+# Copyright (c) 2026, TechVision and contributors
+# License: MIT
 
 from urllib.parse import urlencode
 
-import braintree
 import frappe
+import requests
 from frappe import _
-from frappe.integrations.utils import create_request_log
 from frappe.model.document import Document
-from frappe.utils import call_hook_method, get_url
+from frappe.utils import get_url
 
 from payments.utils import create_payment_gateway
 
 
-class BraintreeSettings(Document):
-	supported_currencies = (
-		"AED",
-		"AMD",
-		"AOA",
-		"ARS",
-		"AUD",
-		"AWG",
-		"AZN",
-		"BAM",
-		"BBD",
-		"BDT",
-		"BGN",
-		"BIF",
-		"BMD",
-		"BND",
-		"BOB",
-		"BRL",
-		"BSD",
-		"BWP",
-		"BYN",
-		"BZD",
-		"CAD",
-		"CHF",
-		"CLP",
-		"CNY",
-		"COP",
-		"CRC",
-		"CVE",
-		"CZK",
-		"DJF",
-		"DKK",
-		"DOP",
-		"DZD",
-		"EGP",
-		"ETB",
-		"EUR",
-		"FJD",
-		"FKP",
-		"GBP",
-		"GEL",
-		"GHS",
-		"GIP",
-		"GMD",
-		"GNF",
-		"GTQ",
-		"GYD",
-		"HKD",
-		"HNL",
-		"HRK",
-		"HTG",
-		"HUF",
-		"IDR",
-		"ILS",
-		"INR",
-		"ISK",
-		"JMD",
-		"JPY",
-		"KES",
-		"KGS",
-		"KHR",
-		"KMF",
-		"KRW",
-		"KYD",
-		"KZT",
-		"LAK",
-		"LBP",
-		"LKR",
-		"LRD",
-		"LSL",
-		"LTL",
-		"MAD",
-		"MDL",
-		"MKD",
-		"MNT",
-		"MOP",
-		"MUR",
-		"MVR",
-		"MWK",
-		"MXN",
-		"MYR",
-		"MZN",
-		"NAD",
-		"NGN",
-		"NIO",
-		"NOK",
-		"NPR",
-		"NZD",
-		"PAB",
-		"PEN",
-		"PGK",
-		"PHP",
-		"PKR",
-		"PLN",
-		"PYG",
-		"QAR",
-		"RON",
-		"RSD",
-		"RUB",
-		"RWF",
-		"SAR",
-		"SBD",
-		"SCR",
-		"SEK",
-		"SGD",
-		"SHP",
-		"SLL",
-		"SOS",
-		"SRD",
-		"STD",
-		"SVC",
-		"SYP",
-		"SZL",
-		"THB",
-		"TJS",
-		"TOP",
-		"TRY",
-		"TTD",
-		"TWD",
-		"TZS",
-		"UAH",
-		"UGX",
-		"USD",
-		"UYU",
-		"UZS",
-		"VEF",
-		"VND",
-		"VUV",
-		"WST",
-		"XAF",
-		"XCD",
-		"XOF",
-		"XPF",
-		"YER",
-		"ZAR",
-		"ZMK",
-		"ZWD",
-	)
+class ChapaSettings(Document):
+    supported_currencies = ("ETB",)
 
-	def validate(self):
-		if not self.flags.ignore_mandatory:
-			self.configure_braintree()
+    def on_update(self):
+        create_payment_gateway(
+            "Chapa",
+            settings="Chapa Settings",
+            controller="Chapa",
+        )
 
-	def on_update(self):
-		create_payment_gateway(
-			"Braintree-" + self.gateway_name,
-			settings="Braintree Settings",
-			controller=self.gateway_name,
-		)
-		call_hook_method("payment_gateway_enabled", gateway="Braintree-" + self.gateway_name)
+        if not self.flags.ignore_mandatory:
+            self.validate_credentials()
 
-	def configure_braintree(self):
-		if self.use_sandbox:
-			environment = "sandbox"
-		else:
-			environment = "production"
+    def validate_credentials(self):
+        """Validate Secret Key by calling Chapa banks endpoint."""
 
-		braintree.Configuration.configure(
-			environment=environment,
-			merchant_id=self.merchant_id,
-			public_key=self.public_key,
-			private_key=self.get_password(fieldname="private_key", raise_exception=False),
-		)
+        if not self.secret_key:
+            return
 
-	def validate_transaction_currency(self, currency):
-		if currency not in self.supported_currencies:
-			frappe.throw(
-				_(
-					"Please select another payment method. Stripe does not support transactions in currency '{0}'"
-				).format(currency)
-			)
+        headers = {
+            "Authorization": f"Bearer {self.get_password('secret_key')}"
+        }
 
-	def get_payment_url(self, **kwargs):
-		return get_url(f"./braintree_checkout?{urlencode(kwargs)}")
+        try:
+            response = requests.get(
+                "https://api.chapa.co/v1/banks",
+                headers=headers,
+                timeout=15,
+            )
 
-	def create_payment_request(self, data):
-		self.data = frappe._dict(data)
+            if response.status_code != 200:
+                frappe.throw(_("Invalid Chapa Secret Key"))
 
-		try:
-			self.integration_request = create_request_log(self.data, service_name="Braintree")
-			return self.create_charge_on_braintree()
+        except requests.RequestException:
+            frappe.throw(_("Unable to connect to Chapa."))
 
-		except Exception:
-			frappe.log_error(frappe.get_traceback())
-			return {
-				"redirect_to": frappe.redirect_to_message(
-					_("Server Error"),
-					_(
-						"There seems to be an issue with the server's braintree configuration. Don't worry, in case of failure, the amount will get refunded to your account."
-					),
-				),
-				"status": 401,
-			}
+    def validate_transaction_currency(self, currency):
+        if currency not in self.supported_currencies:
+            frappe.throw(
+                _("Chapa only supports transactions in ETB.")
+            )
 
-	def create_charge_on_braintree(self):
-		self.configure_braintree()
+    def get_payment_url(self, **kwargs):
+        return get_url(f"./chapa_checkout?{urlencode(kwargs)}")
 
-		redirect_to = self.data.get("redirect_to") or None
-		redirect_message = self.data.get("redirect_message") or None
+    def create_request(self, data):
+        """
+        Called by Payment Request.
+        """
 
-		result = braintree.Transaction.sale(
-			{
-				"amount": self.data.amount,
-				"payment_method_nonce": self.data.payload_nonce,
-				"options": {"submit_for_settlement": True},
-			}
-		)
+        self.data = frappe._dict(data)
 
-		if result.is_success:
-			self.integration_request.db_set("status", "Completed", update_modified=False)
-			self.flags.status_changed_to = "Completed"
-			self.integration_request.db_set("output", result.transaction.status, update_modified=False)
+        return self.initialize_transaction()
 
-		elif result.transaction:
-			self.integration_request.db_set("status", "Failed", update_modified=False)
-			error_log = frappe.log_error(
-				"code: "
-				+ str(result.transaction.processor_response_code)
-				+ " | text: "
-				+ str(result.transaction.processor_response_text),
-				"Braintree Payment Error",
-			)
-			self.integration_request.db_set("error", error_log.error, update_modified=False)
-		else:
-			self.integration_request.db_set("status", "Failed", update_modified=False)
-			for error in result.errors.deep_errors:
-				error_log = frappe.log_error(
-					"code: " + str(error.code) + " | message: " + str(error.message),
-					"Braintree Payment Error",
-				)
-				self.integration_request.db_set("error", error_log.error, update_modified=False)
+    def initialize_transaction(self):
+        headers = {
+            "Authorization": f"Bearer {self.get_password('secret_key')}",
+            "Content-Type": "application/json",
+        }
 
-		if self.flags.status_changed_to == "Completed":
-			status = "Completed"
-			if self.data.reference_doctype and self.data.reference_docname:
-				custom_redirect_to = None
-				try:
-					custom_redirect_to = frappe.get_doc(
-						self.data.reference_doctype, self.data.reference_docname
-					).run_method("on_payment_authorized", self.flags.status_changed_to)
-					braintree_success_page = frappe.get_hooks("braintree_success_page")
-					if braintree_success_page:
-						custom_redirect_to = frappe.get_attr(braintree_success_page[-1])(self.data)
-				except Exception:
-					frappe.log_error(frappe.get_traceback())
+        payload = {
+            "amount": str(self.data.amount),
+            "currency": self.data.currency,
+            "email": self.data.payer_email,
+            "first_name": self.data.payer_name,
+            "tx_ref": self.data.reference_docname,
+            "callback_url": get_url(
+                "/api/method/payments.payment_gateways.doctype.chapa_settings.chapa_settings.chapa_callback"
+            ),
+            "return_url": get_url(
+                "/payment-success"
+            ),
+            "customization": {
+                "title": frappe.db.get_single_value(
+                    "Website Settings",
+                    "app_name"
+                ),
+                "description": self.data.description,
+            },
+        }
 
-				if custom_redirect_to:
-					redirect_to = custom_redirect_to
+        try:
+            response = requests.post(
+                "https://api.chapa.co/v1/transaction/initialize",
+                json=payload,
+                headers=headers,
+                timeout=30,
+            )
 
-			redirect_url = "payment-success"
-		else:
-			status = "Error"
-			redirect_url = "payment-failed"
+            response.raise_for_status()
 
-		get_parameters = [("doctype", self.data.reference_doctype), ("docname", self.data.reference_docname)]
-		if redirect_to:
-			get_parameters.append(("redirect_to", redirect_to))
-		if redirect_message:
-			get_parameters.append(("redirect_message", redirect_message))
+            result = response.json()
 
-		redirect_url += "?" + urlencode(get_parameters)
-		return {"redirect_to": redirect_url, "status": status}
+            if result.get("status") != "success":
+                frappe.throw(result.get("message"))
 
+            return {
+                "redirect_to": result["data"]["checkout_url"],
+                "status": "Initialized",
+            }
 
-def get_gateway_controller(doc):
-	payment_request = frappe.get_doc("Payment Request", doc)
-	return frappe.db.get_value("Payment Gateway", payment_request.payment_gateway, "gateway_controller")
-
-
-def get_client_token(doc):
-	gateway_controller = get_gateway_controller(doc)
-	settings = frappe.get_doc("Braintree Settings", gateway_controller)
-	settings.configure_braintree()
-
-	return braintree.ClientToken.generate()
+        except Exception:
+            frappe.log_error(frappe.get_traceback(), "Chapa Initialize")
+            frappe.throw(_("Unable to initialize Chapa payment."))
