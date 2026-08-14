5ccb283c390cd6b59173fbc9929adc6d626349ca
mehairwamlake <mehariwamlake@gmail.com>
2026-08-14 23:38:16 +0300

    chapa verfication fix

--- payments/payment_gateways/doctype/chapa_settings/chapa.py
+++ payments/payment_gateways/doctype/chapa_settings/chapa.py
@@ -2,14 +2,26 @@ import frappe
 
 
 class Chapa:
-	def __init__(self):
-		self.settings = frappe.get_cached_doc("Chapa Settings")
+    def __init__(self):
+        settings_name = frappe.db.get_value(
+            "Chapa Settings",
+            {"gateway_name": "Chapa"},
+            "name"
+        )
 
-	def validate_transaction_currency(self, currency):
-		self.settings.validate_transaction_currency(currency)
+        if not settings_name:
+            frappe.throw("Chapa Settings configuration not found")
 
-	def get_payment_url(self, **kwargs):
-		return self.settings.get_payment_url(**kwargs)
+        self.settings = frappe.get_cached_doc(
+            "Chapa Settings",
+            settings_name
+        )
 
-	def create_request(self, data):
-		return self.settings.create_request(data)
+    def validate_transaction_currency(self, currency):
+        self.settings.validate_transaction_currency(currency)
+
+    def get_payment_url(self, **kwargs):
+        return self.settings.get_payment_url(**kwargs)
+
+    def create_request(self, data):
+        return self.settings.create_request(data)
